ae43f88ee99838962b16e0efe24ae371b6ff14c9
supreeth1816 <supreet.venkata@gmail.com>
2020-11-02 02:19:45 +0530

    Added App Context

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'app_context.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<StatefulWidget> createState() {
+    return _HomeScreen();
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreen extends State<HomeScreen> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Provider.of<AppContext>(context, listen: false).initializeContext();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "ISA Image Encryption"
+        ),
+        actions: [
+
+        ],
+      ),
+      resizeToAvoidBottomInset: false,
+      body: ScreenAdapter(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+            child: ListView(
+              children: <Widget>[
+                SizedBox(
+                  height: 10.0,
+                ),
+                HomeScreenDonkeyLogo(),
+                SizedBox(
+                  height: 10.0,
+                ),
+                HomeScreenMessageLogo(),
+                SizedBox(
+                  height: 5.0,
+                ),
+                HomeScreenStartEncodeBtn(),
+                SizedBox(
+                  height: 5.0,
+                ),
+                HomeScreenStartDecodeBtn(),
+                SizedBox(
+                  height: 5.0,
+                ),
+                HomeScreenContribBtn(),
+              ],
+            ),
+          )),
+    );
   }
 }
---
+++ lib/states.dart
@@ -0,0 +1,27 @@
+
+enum AppRunningState {
+  INTEGRATION_TEST,
+  UNIT_TEST,
+  DEVELOPMENT,
+  PRODUCTION,
+}
+
+enum PlatformState {
+  IPAD,
+  IPHONE,
+  GENERIC,
+}
+
+
+enum DecodeResultState {
+  SUCCESS,
+  ERROR,
+}
+
+
+enum LoadingState {
+  LOADING,
+  ERROR,
+  PENDING,
+  SUCCESS,
+}
